8b3eb298ad4b3b0cf37ea5bf6a8bce490bbb510c
Harshksaw <mister.harshkumar@gmail.com>
2026-02-06 06:07:41 +0000

    feat: Remove modular structure for users, items, and auth by deleting their models, services, and API routes, and updating core API and dependency files.

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,14 +1,8 @@
 from fastapi import APIRouter
 
-from app.api.routes import items, login, private, users, utils
-from app.core.config import settings
+from app.modules.market import api as market
+from app.modules.inference import api as inference
 
 api_router = APIRouter()
-api_router.include_router(login.router)
-api_router.include_router(users.router)
-api_router.include_router(utils.router)
-api_router.include_router(items.router)
-
-
-if settings.ENVIRONMENT == "local":
-    api_router.include_router(private.router)
+api_router.include_router(market.router)
+api_router.include_router(inference.router)
